00905e83de147415d6f355f277908c7c43272083
MedoMous <163560484+MedoMous@users.noreply.github.com>
2025-12-29 10:28:24 +0100

    assests folder issue is now fixed

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -4,7 +4,7 @@ const Search = ({searchTerm , setSearchTerm} ) => {
     return(
         <div className = "search">
             <div>
-                <img src="../../assets/public/search.svg" alt="search"/>
+                <img src="../public/search.svg" alt="search"/>
                 <input
                     type="text"
                     placeholder="Search for a movie"
